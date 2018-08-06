2c08e23272af3b3146830f881d861ce07540875e
ashishk144 <ashishk144>
2018-08-06 21:01:17 +0530

    " -> 9 of 10 passed. pylint: 9.62/10 "

--- M7/Functions - Assignment-3/assignment3.py
+++ M7/Functions - Assignment-3/assignment3.py
@@ -26,6 +26,5 @@ def main():
     data = data.split(' ')
     data = list(map(float, data))
     print("Lowest Payment: " +str(paying_debtoffinayear(data[0], data[1])))
-    
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
