f38c75af39805bd0cd32e9f1378fd00398b5f400
ashishk144 <ashishk144>
2018-08-07 17:41:22 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- M8/p2/assignment2.py
+++ M8/p2/assignment2.py
@@ -1,8 +1,9 @@
-# Exercise: Assignment-2
-# Write a Python function, sumofdigits, that takes in one number and returns the sum of digits of given number.
+""" Exercise: Assignment-2
+Write a Python function, sumofdigits, that takes in one number and
+returns the sum of digits of given number.
 
-# This function takes in one number and returns one number.
-
+This function takes in one number and returns one number.
+"""
 
 def sumofdigits(num_ber):
     '''
@@ -15,6 +16,7 @@ def sumofdigits(num_ber):
     return (num_ber % 10) + sumofdigits(num_ber // 10)
 
 def main():
+	"""defining main"""
     in_put = input()
     print(sumofdigits(int(in_put)))  
 
