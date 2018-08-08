e1918bef467c360bb1f089958e11dd61906bc420
ashishk144 <ashishk144>
2018-08-08 18:31:38 +0530

    " -> 9 of 9 passed. pylint: 9.58/10 "

--- M9/p1/assignment1.py
+++ M9/p1/assignment1.py
@@ -44,3 +44,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    """ s = secret_word
+    l = list of guessed words
+    for i in l:
+        if i in s:
+            s = s.replace(i, "")
+    if s == '':
+        return True
+    return False
+    """
