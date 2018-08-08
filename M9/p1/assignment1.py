ebb3a7d5bc21267d9872aff0f36bb6bc3f576012
ashishk144 <ashishk144>
2018-08-08 18:18:37 +0530

    " -> 0 of 9 passed. pylint: 8.75/10 "

--- M9/p1/assignment1.py
+++ M9/p1/assignment1.py
@@ -18,7 +18,7 @@ def is_word_guessed(secret_word, letters_guessed):
     s = list(secret_word)
     for i in range(0, 8):
         for [j, _] in enumerate(secret_word):
-            if letters_guessed == secret_word[j]:
+            if letters_guessed[i] == secret_word[j]:
                 s[j] = secret_word[j]
     
     s = ''.join(s)
