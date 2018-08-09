d07e6d9e16b77951facbef4612f5d911b831a3ac
ashishk144 <ashishk144>
2018-08-09 15:01:13 +0530

    " -> 0 of 10 passed. pylint: 6.88/10 "

--- M10/p1/assignment1.py
+++ M10/p1/assignment1.py
@@ -12,7 +12,9 @@ def get_available_letters(letters_guessed):
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    compare_string = 'abcdefghijklmnopqrstuvwxyz'
+    #compare_string = 'abcdefghijklmnopqrstuvwxyz'
+    import string
+    compare_string = string.ascii_lowercase()
     for ch_ar in letters_guessed:
         if ch_ar in compare_string:
             compare_string = compare_string.replace(ch_ar, '')
