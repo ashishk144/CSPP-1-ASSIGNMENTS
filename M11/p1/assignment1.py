d155973d02f3f91bd063c317b1a15c764d7ba169
ashishk144 <ashishk144>
2018-08-10 18:25:10 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -1,6 +1,8 @@
 '''
 Exercise: Assignment-1
-The first step is to implement some code that allows us to calculate the score for a single word. The function get_word_score should accept as input a string of lowercase letters (a word) and return the integer score for that word, using the game's scoring rules.
+The first step is to implement some code that allows us to calculate the score for a single word.
+The function get_word_score should accept as input a string of lowercase letters (a word)
+and return the integer score for that word, using the game's scoring rules.
 '''
 
 def get_word_score(word_entered, len_gth):
@@ -20,9 +22,9 @@ def get_word_score(word_entered, len_gth):
     """
     # TO DO ... <-- Remove this comment when you code this function
     scrabble_letters = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+        'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+        'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+        's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     score = 0
     for char_acters in word_entered:
         if char_acters in scrabble_letters:
