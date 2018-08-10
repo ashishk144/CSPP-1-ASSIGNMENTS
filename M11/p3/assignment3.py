3cd435842f3a7522f9ff027b510e812ea887acda
ashishk144 <ashishk144>
2018-08-10 19:38:06 +0530

    " -> 3 of 3 passed. pylint: 9.52/10 "

--- M11/p3/assignment3.py
+++ M11/p3/assignment3.py
@@ -2,7 +2,8 @@
 '''
 At this point, we have written code to generate a random hand and display that hand to the user.
 We can also ask the user for a word (Python's input) and score the word (using your getWordScore).
-However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game.
+However, at this point we have not written any code to verify that a word given by a player
+obeys the rules of the game.
 A valid word is in the word list; and it is composed entirely of letters from the current hand.
 Implement the is_validword function.
 
@@ -11,8 +12,8 @@ In addition, you will want to test your implementation by calling it multiple ti
 what should the correct behavior be? Additionally, the empty string ('') is not a valid word -
 if you code this function correctly, you shouldn't need an additional check for this condition.
 
-Fill in the code for is_validword in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py
-before pasting your function definition here.
+Fill in the code for is_validword in ps4a.py and be sure you've passed the appropriate tests in
+test_ps4a.py before pasting your function definition here.
 '''
 
 def is_validword(word_input, hand_let, word_list):
@@ -36,6 +37,7 @@ def is_validword(word_input, hand_let, word_list):
     return False
 
 def main():
+    '''input function'''
     word_inp = input()
     hand_len = int(input())
     a_dict = {}
