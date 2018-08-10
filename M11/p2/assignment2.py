32175017d8e1e67e89b419f8775d12261c992b68
ashishk144 <ashishk144>
2018-08-10 19:08:24 +0530

    " -> 6 of 6 passed. pylint: 0.00/10 "

--- M11/p2/assignment2.py
+++ M11/p2/assignment2.py
@@ -1,5 +1,7 @@
-#Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+"""Exercise: Assignment-2
+Implement the updateHand function. Make sure this function has no side effects: i.e.,
+it must not mutate the hand passed in. Before pasting your function definition here,
+be sure you've passed the appropriate tests in test_ps4a.py."""
 
 
 def updateHand(hand_inp, word_inp):
@@ -7,7 +9,7 @@ def updateHand(hand_inp, word_inp):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
@@ -15,7 +17,7 @@ def updateHand(hand_inp, word_inp):
     Has no side effects: does not modify hand.
 
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
@@ -24,16 +26,15 @@ def updateHand(hand_inp, word_inp):
     return hand_inp
 
 def main():
-	n = input()
-	adict = {}
-	for i in range(int(n)):
+	"""input function"""
+	in_put = input()
+	a_dict = {}
+	for _ in range(int(in_put)):
 		data = input()
 		l = data.split()
-		adict[l[0]] = int(l[1])
+		a_dict[l[0]] = int(l[1])
 	data1 = input()
-	print(updateHand(adict,data1))
-		
-
+	print(updateHand(a_dict, data1))
 
 if __name__== "__main__":
 	main()
