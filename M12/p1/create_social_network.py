6a801491ffb97688c5540daa6234ecb6748b46e8
ashishk144 <ashishk144>
2018-08-11 09:52:22 +0530

    " -> 0 of 3 passed. pylint: 8.12/10 "

---
+++ M12/p1/create_social_network.py
@@ -0,0 +1,55 @@
+'''
+    Assignment-1 Create Social Network
+'''
+net_dict = {}
+def create_social_network(inp_line):
+    '''
+    The data argument passed to the function is a string
+    It represents simple social network data
+    In this social network data there are people following other people
+
+    Here is an example social network data string:
+    John follows Bryant,Debra,Walter
+    Bryant follows Olive,Ollie,Freda,Mercedes
+    Mercedes follows Walter,Robin,Bryant
+    Olive follows John,Ollie
+
+    The string has multiple num_lines and each line represents one person
+    The first word of each line is the name of the person
+    The second word is follows that separates the person from the followers
+    After the second word is a list of people separated by ,
+
+    create_social_network function should split the string on lines
+    then extract the person and the followers by splitting each line
+    finally add the person and the followers to a dictionary and
+    return the dictionary
+
+    Caution: watch out for trailing spaces while splitting the string.
+    It may cause your test cases to fail although your output may be right
+
+    Error handling case:
+    Return a empty dictionary if the string format of the data is invalid
+    Empty dictionary is not None, it is a dictionary with no keys
+    '''
+
+    # remove the pass below and start writing your code
+    inp_line = inp_line.split("\n")
+    for i in inp_line:
+        inp_line = inp_line.split(' follows ')
+        net_dict[0] = net_dict[1].split(',') 
+
+def main():
+    '''
+        handling testcase input and printing output
+    '''
+    store_string = ''
+    num_lines = int(input())
+    for i in range(num_lines):
+        i += 1
+        store_string += input()
+        store_string += '\n'
+
+    print(create_social_network(store_string))
+
+if __name__ == "__main__":
+    main()
