1ae6dc105be5709453ab295b7ea0a173969d7f66
ashishk144 <ashishk144>
2018-08-17 15:19:01 +0530

    " -> 1 of 3 passed. pylint: 9.07/10 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -49,7 +49,7 @@ def similarity(dict1, dict2):
         elif key_s in new_dict1 and key_s not in new_dict2:
             freq_dict[key_s] = [new_dict1[key_s], 0]
         elif key_s in new_dict2 and key_s not in new_dict1:
-            freq_dict[key_s] = [0, new_dict2[key_s]]
+            freq_dict[key_s] = [0, new_dict2[key_s]]    
     numerator = 0
     deno_1 = 0
     deno_2 = 0
