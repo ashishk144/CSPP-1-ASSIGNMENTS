e913dabe3d877d5f9bd0a3664da844703ad48471
ashishk144 <ashishk144>
2018-08-17 11:12:06 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -5,7 +5,11 @@ def word_list(inp_1):
     new_dict = {}
     line_1 = inp_1
     line_1 = line_1.lower()
-    line_1 = line_1.replace('.', '').replace(',', '').replace('?', '')
+    line_1 = line_1.replace('.', '').replace(',', '').replace('?', '')\
+    replace('0', '').replace('1', '').replace('2', '')\
+    replace('3', '').replace('4', '').replace('5', '')\
+    replace('6', '').replace('7', '').replace('8', '')\
+    replace('9', '')
     line_1 = line_1.split(' ')
     # line_1 = line_1.split('\n')
     # print(line_1)
@@ -23,7 +27,17 @@ def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    return word_list(dict1), word_list(dict2)
+    new_dict1 = word_list(dict1)
+    new_dict2 = word_list(dict2)
+    stop_words = load_stopwords(stopwords.txt)
+    for word_s in stop_words:
+        if word_s in new_dict1:
+            del new_dict1[word_s]
+    
+    for word_s in stop_words:
+        if word_s in new_dict2:
+            del new_dict2[word_s]
+    print(new_dict1, new_dict2)
 
 def load_stopwords(filename):
     '''
@@ -33,7 +47,6 @@ def load_stopwords(filename):
     with open(filename, 'r') as filename:
         for line in filename:
             stopwords[line.strip()] = 0
-        print(stopwords)
     return stopwords
 
 def main():
