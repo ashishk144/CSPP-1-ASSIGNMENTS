ec22526c0015c8b9d5325ce0ba27b481b7bb8b5c
ashishk144 <ashishk144>
2018-08-17 16:01:21 +0530

    " -> 0 of 3 passed. pylint: 8.75/10 "

--- M16/CodeCampDocumentDistance/doc_dict.py
+++ M16/CodeCampDocumentDistance/doc_dict.py
@@ -6,15 +6,15 @@ import re
 import math
 
 def string_opr(inp_str):
-    # inp_str = inp_str.lower()
-    # regex = re.compile('[^a-z]')
-    # inp_str = regex.sub('', inp_str)
-    # list_ofwords = inp_str.split(' ')
-    # for index in range(len(list_ofwords)):
-    #     list_ofwords[index] = list_ofwords[index].strip()
-    # return list_ofwords
+    inp_str = inp_str.lower()
     regex = re.compile('[^a-z]')
-    return(regex.sub('', eachword.strip()) for eachword in inp_str.lower().split(' '))
+    inp_str = regex.sub('', inp_str)
+    list_ofwords = inp_str.split(' ')
+    for index in range(len(list_ofwords)):
+        list_ofwords[index] = list_ofwords[index].strip()
+    return list_ofwords
+    # regex = re.compile('[^a-z]')
+    # return(regex.sub('', eachword.strip()) for eachword in inp_str.lower().split(' '))
 def remove_stopword(word_list):
     stop_words = load_stopwords('stopwords.txt')
     for each_word in stop_words:
