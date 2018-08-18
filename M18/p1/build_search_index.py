07bd66062229ed39b4d288939b7f81d5d92b8e41
ashishk144 <ashishk144>
2018-08-18 10:42:38 +0530

    " -> 0 of 1 passed. pylint: 9.79/10 "

--- M18/p1/build_search_index.py
+++ M18/p1/build_search_index.py
@@ -62,9 +62,9 @@ def word_freq(list_word, ind, doc_id, diction):
     for each_wrd in list_word:
         if each_wrd != '':
             if each_wrd not in diction:
-                print(each_wrd)
-                diction[each_wrd] = (doc_id, 0)
-            diction[each_wrd][ind][1] += 1
+                # print(each_wrd)
+                diction[each_wrd] = [(doc_id, each_wrd.count(list_word))]
+            # diction[each_wrd][ind][1] += 1
     return diction
 
 def build_search_index(docs):
