0725ef71300ebc31fd87f608a335b29bb040c2c5
ashishk144 <ashishk144>
2018-08-18 10:14:00 +0530

    " -> 0 of 1 passed. pylint: 5.41/10 "

--- M18/p1/build_search_index.py
+++ M18/p1/build_search_index.py
@@ -57,7 +57,7 @@ def remove_words(list_ofwords):
             print(each_list)
             while each_word in each_list:
                 each_list.remove(each_word)
-    # print(list_ofwords)
+    print(list_ofwords)
     return list_ofwords
 
 def build_search_index(docs):
@@ -80,7 +80,7 @@ def build_search_index(docs):
 
 # helper function to print the search index
 # use this to verify how the search index looks
-def print_search_index(index):
+# def print_search_index(index):
     '''
         print the search index
     '''
