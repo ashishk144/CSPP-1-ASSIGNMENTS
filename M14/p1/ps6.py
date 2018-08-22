d58860514f691fe9c07cacf00064a37bb3d13bd0
ashishk144 <ashishk144>
2018-08-22 21:16:34 +0530

    " -> 0 of 5 passed. pylint: 4.03/10 "

--- M14/p1/ps6.py
+++ M14/p1/ps6.py
@@ -247,10 +247,10 @@ class CiphertextMessage(Message):
 
 #Example test case (PlaintextMessage)
 plaintext = PlaintextMessage('hello', 2)
-# print('Expected Output: jgnnq')
-# print('Actual Output:', plaintext.get_message_text_encrypted())
+print('Expected Output: jgnnq')
+print('Actual Output:', plaintext.get_message_text_encrypted())
     
 # Example test case (CiphertextMessage)
 ciphertext = CiphertextMessage('jgnnq')
-# print('Expected Output:', (24, 'hello'))
-# print('Actual Output:', ciphertext.decrypt_message())
+print('Expected Output:', (24, 'hello'))
+print('Actual Output:', ciphertext.decrypt_message())
