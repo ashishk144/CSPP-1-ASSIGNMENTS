c6317a50a8dfb2087d62127111e1d2807e87e178
ashishk144 <ashishk144>
2018-08-24 13:49:36 +0530

    " -> 0 of 14 passed. pylint: 9.29/10 "

---
+++ M21/CodeCampTicTacToe/tic_tac_toe.py
@@ -0,0 +1,50 @@
+'''Tic Tac Toe'''
+
+def check_rows(matrix_row):
+    store_val = []
+    for _, j in enumerate(matrix_row):
+        if j[0] == j[1] and j[1] == j[2]:
+            store_val.append(j[0])
+    if len(store_val) == 1:
+        return store_val[0]
+    return 0
+
+def check_cols(matrix_col):
+    store_val = []
+    for i in range(1, len(matrix_col)):
+        count = 0
+        for j in range(len(matrix_col[i])):
+            if matrix_col[j][i-1] == matrix_col[j][i]:
+                count += 0
+        if count == 2:
+            store_val.append(matrix_col[j][i])
+    if len(store_val) == 1:
+        return store_val[0]
+    return 0
+
+
+def main():
+    '''Taking input and delivering output'''
+    new_matrix = []
+    correct_count = 0
+    out_put = ''
+    for _ in range(3):
+        line = input()
+        new_matrix.append(line.split(' '))
+    for each_list in new_matrix:
+        if 'x' not in each_list or 'o' not in each_list or '.' not in each_list:
+            print("Invalid Input")
+            break
+    if check_rows(new_matrix):
+        correct_count += 1
+        out_put += check_rows(new_matrix)
+    if check_cols(new_matrix):
+        correct_count += 1
+        out_put += check_cols(new_matrix)
+
+    if correct_count == 1:
+        print(out_put)
+    else:
+        print('Invalid Game')
+if __name__ == '__main__':
+    main()
