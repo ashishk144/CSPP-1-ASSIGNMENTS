10e943443abb24702dc7f5f939531c14864e3bf0
ashishk144 <ashishk144>
2018-08-24 18:06:07 +0530

    " -> 11 of 14 passed. pylint: 8.39/10 "

--- M21/CodeCampTicTacToe/tic_tac.py
+++ M21/CodeCampTicTacToe/tic_tac.py
@@ -42,7 +42,6 @@ def diagonal_ltor(game_grid):
     win_flag = game_grid[0][0]
     win_count = 0
     if win_flag != '.':
-        continue
         for i in range(grid_len):
             if game_grid[i][i] == win_flag:
                 win_count += 1
@@ -55,7 +54,6 @@ def diagonal_rtol(game_grid):
     win_flag = game_grid[0][-1]
     win_count = 0
     if win_flag != '.':
-        continue
         for i in range(grid_len):
             if game_grid[i][-i-1] == win_flag:
                 win_count += 1
