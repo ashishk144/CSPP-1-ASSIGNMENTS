6d3a0596768c061c68f666a352130fa1cbebe703
ashishk144 <ashishk144>
2018-08-25 11:04:43 +0530

    " -> 0 of 4 passed. pylint: 6.84/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -5,19 +5,26 @@ each word
 import re
 
 def tokenize(string):
+    '''Converting given strings to a dictionary with frequencies of each word'''
     reg = re.compile("[^a-z A-Z]")
-    token_dict ={}
+    token_dict = {}
     line = []
     for each_line in string:
-    	line.append(reg.sub('', each_line))
-    print(line)
+        line.append(reg.sub('', each_line))
+    # print(line)
+    for each_list in line:
+        for each_word in each_list:
+            token_dict[each_word] = token_dict.get(each_word, 0) + 1
+    return token_dict
 
 def main():
-    n = int(input())
+    '''Taking inputs and displaying outputs'''
+    num_lines = int(input())
     list_ofstrs = []
     for _ in range(n):
-    	list_ofstrs.append(input())
+        list_ofstrs.append(input())
     # print(list_ofstrs)
-    tokenize(list_ofstrs)
+    print(tokenize(list_ofstrs))
 if __name__ == '__main__':
     main()
+ 
