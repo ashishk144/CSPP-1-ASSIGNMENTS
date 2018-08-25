a9b5456170236fe937afd34bf4746c562570f305
ashishk144 <ashishk144>
2018-08-25 12:46:40 +0530

    " -> 26 of 26 passed. pylint: 10.00/10 "

--- M22/Check Sudoku/check_sudoku.py
+++ M22/Check Sudoku/check_sudoku.py
@@ -40,9 +40,9 @@ def check_sudoku(sudoku):
                 for j in range(k, k+3):
                     # print(sudoku[j][i])
                     list_2.append(sudoku[j][i])
-            print(list_2)
+            # print(list_2)
             list_2.sort()
-            print(list_2)
+            # print(list_2)
             if ''.join(list_2) != '123456789':
                 return False
             count_ofloops += 1
