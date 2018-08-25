5c2a5cae8e52f558d1914ee09cd58a94a19f7687
ashishk144 <ashishk144>
2018-08-25 11:57:25 +0530

    " -> 16 of 26 passed. pylint: 9.58/10 "

--- M22/Check Sudoku/check_sudoku.py
+++ M22/Check Sudoku/check_sudoku.py
@@ -23,7 +23,7 @@ def check_sudoku(sudoku):
         list_1 = []
         for j in range(9):
             # print(j,i)
-            # print(sudoku[j][i])
+            print(sudoku[j][i])
             list_1.append(sudoku[j][i])
             # print(list_1)
         list_1.sort()
