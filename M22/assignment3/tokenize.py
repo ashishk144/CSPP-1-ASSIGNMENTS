f0f0155a3dde4d4949f355102e54722da9f46486
ashishk144 <ashishk144>
2018-08-25 11:06:01 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -12,7 +12,7 @@ def tokenize(string):
     for each_line in string:
         line.append(reg.sub('', each_line))
     # print(line)
-    for each_list in line:
+    for each_word in line:
         # for each_word in each_list:
         token_dict[each_word] = token_dict.get(each_word, 0) + 1
     return token_dict
