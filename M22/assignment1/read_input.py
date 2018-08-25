4344856eb92534f1634fb120337a8dad40a4197c
ashishk144 <ashishk144>
2018-08-25 10:32:28 +0530

    " -> 0 of 2 passed. pylint: 10.00/10 "

---
+++ M22/assignment1/read_input.py
@@ -0,0 +1,17 @@
+'''
+Write a python program to read multiple lines of text input and
+store the input into a string.
+'''
+
+def main():
+    '''Defining a function to read multiple lines and display them'''
+    num_lines = int(input())
+    line = []
+    for i in range(num_lines):
+        line[i] = input().split('\n')
+
+    for each_line in line:
+        print(each_line)
+
+if __name__ == '__main__':
+    main()
